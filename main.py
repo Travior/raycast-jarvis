da0801c405351af88e37daade00a469498edc220
Travior <30752199+Travior@users.noreply.github.com>
2023-03-04 15:24:47 +0100

    Implement basic server structure

---
+++ main.py
@@ -0,0 +1,16 @@
+import importlib
+from flask import Flask
+from flask import Blueprint
+import os
+
+app = Flask(__name__)
+
+for filename in os.listdir("jarvis_modules"):
+    if filename.endswith(".py") and not filename.endswith("__init__.py"):
+        module = importlib.import_module(f"jarvis_modules.{filename[:-3]}")
+        for name in dir(module):
+            if isinstance(getattr(module, name), Blueprint):
+                app.register_blueprint(getattr(module, name))
+
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port=3000)
