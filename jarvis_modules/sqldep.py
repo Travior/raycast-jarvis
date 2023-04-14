66078c848c2e3b2147f3940d159ee5950e7bbb6e
Travior <30752199+Travior@users.noreply.github.com>
2023-04-14 12:24:26 +0200

    Change sqldep route to use sqllineage

--- jarvis_modules/sqldep.py
+++ jarvis_modules/sqldep.py
@@ -1,13 +1,24 @@
 from flask import Blueprint
 from flask import request
-from sqlglot import parse_one, exp
+from sqllineage.runner import LineageRunner
 
 sqldep_bp = Blueprint("sqldep", __name__)
+
 
 @sqldep_bp.route("/sqldep")
 def get_dependencies():
     data = request.get_json(silent=True)
     if data is not None and "sql" in data.keys():
-        tables = parse_one(data['sql']).find_all(exp.Table)
-        return [table.name for table in tables]
+        response = {"number_of_statements": 0, "dependencies": []}
+        result = LineageRunner(data["sql"])
+        response["number_of_statements"] = len(result.statements())
+        for statement in result.statements():
+            result = LineageRunner(statement)
+            response["dependencies"].append(
+                {
+                    "statement": statement,
+                    "tables": [str(t) for t in result.source_tables],
+                }
+            )
+        return response
     return ("bad request", 400)
